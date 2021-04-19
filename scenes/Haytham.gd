cd78c8cb4d9c9fac52dcc4f62c61393924dd4e69
Saraiva <joao_saraiva@edu.unifor.br>
2021-04-19 18:54:32 -0300

    v3

--- scenes/Haytham.gd
+++ scenes/Haytham.gd
@@ -1,6 +1,42 @@
 extends KinematicBody2D
 
+export var gravity = 10
+export var walk_speed = 120
+export var jump_speed = -300
+export var movement = Vector2(0,0)
 
+func _physics_process(delta):
+	if is_on_ceiling():
+		movement.y = 0
+	if !is_on_floor():
+		movement.y+= gravity
+	else:
+		movement.y = gravity
+		
+	var horizontal_axis = Input.get_action_strength("right")-Input.get_action_strength("left")
+	
+	movement.x = horizontal_axis*walk_speed
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		movement.y = jump_speed
+		
+	move_and_slide(movement,Vector2.UP)
+	update_animations()
+	
+func update_animations():
+	if movement.x>0 :
+		$AnimatedSprite.scale.x = 2
+	elif movement.x<0:
+		$AnimatedSprite.scale.x = -2
+		
+		
+		if is_on_floor():
+			if abs(movement.x)>0:
+				$AnimatedSprite.play("walking")
+			else:
+				$AnimatedSprite.play("idle")
+		else:
+			$AnimatedSprite.play("jump")
+			
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
