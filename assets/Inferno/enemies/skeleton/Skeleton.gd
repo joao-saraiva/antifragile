6c71d9ec004c9a999e8d64001e6fd9803ea16248
joao-saraiva <joao_saraiva@edu.unifor.br>
2021-04-21 19:40:11 -0300

    IA.2

--- assets/Inferno/enemies/skeleton/Skeleton.gd
+++ assets/Inferno/enemies/skeleton/Skeleton.gd
@@ -7,19 +7,35 @@ var react_time = 100
 var direction = 0
 var next_direction = 0
 var next_direction_time = 0
+var last_animation = ""
 onready var player = get_parent().get_node("Haytham")
 
+func _ready():
+	if not is_attacking():
+		$AnimatedSprite.play("walk")
+	last_animation = $AnimatedSprite.animation
 func _physics_process(delta):
+	
+	
+	
 	if player.position.x < position.x and next_direction != -1:
 		next_direction = -1
-		$AnimatedSprite.scale.x*=-1
+		$AnimatedSprite.scale.x=-1
+		$AttackSkeleton/Attack.scale.x = -1
+		$AttackSkeleton/Attack.position.x = -12.929
+		$DetectedPlayer/Detected.scale.x = -1
+		$DetectedPlayer/Detected.position.x = -24.975
 		next_direction_time = OS.get_ticks_msec()+react_time
 		
 		
 		
 	elif player.position.x > position.x and next_direction != 1:
 		next_direction = 1
-		$AnimatedSprite.scale.x*=-1
+		$AnimatedSprite.scale.x=1
+		$AttackSkeleton/Attack.scale.x = 1
+		$AttackSkeleton/Attack.position.x = 12.929
+		$DetectedPlayer/Detected.scale.x = 1
+		$DetectedPlayer/Detected.position.x = 24.975
 		next_direction_time = OS.get_ticks_msec()+react_time
 		
 		
@@ -38,3 +54,37 @@ func _physics_process(delta):
 	
 	
 	
+
+func is_attacking():
+	return last_animation == "attack" and $AnimatedSprite.frame !=5
+	pass
+
+
+
+
+func _on_DetectedPlayer_body_entered(body):
+	if(body.get_name() == "Haytham"):
+		$AnimatedSprite.play("attack")
+		
+		
+		$DetectedPlayer/Timer.start()
+		$AttackSkeleton.monitoring = true
+		
+
+
+	
+
+func _on_Timer_timeout():
+	$AttackSkeleton.monitoring = false
+	
+	pass # Replace with function body.
+
+
+func _on_AttackSkeleton_body_entered(body):
+	player.life -=1
+	if player.position.x < position.x:
+		player.position.x -= 30
+	else:
+		player.position.x += 30
+	print(player.life) 
+	pass # Replace with function body.
