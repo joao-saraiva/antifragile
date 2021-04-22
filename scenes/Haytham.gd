92db0788ab3bfbd6d6ad84eaa057ff192beffd05
Davi Viana <davi.vc@hotmail.com>
2021-04-22 17:58:09 -0300

    player movements update

--- scenes/Haytham.gd
+++ scenes/Haytham.gd
@@ -1,13 +1,15 @@
 extends KinematicBody2D
 
 export var gravity = 10
-export var walk_speed = 120
+export var run_speed = 140
+export var walk_speed = 80
 export var jump_speed = -300
 export var movement = Vector2(0,0)
 var mouse_position = Vector2(0,0)
 var last_movement_y = 0
 var last_animation = ""
 var is_dead = false
+var is_running = false
 var esp32Esquerda = false
 var esp32Direita = false
 
@@ -48,13 +50,17 @@ func _physics_process(delta):
 	else:
 		movement.y = gravity
 	
-	if not is_dead:	
-		if Input.is_action_just_pressed("attack_strike") and not is_landing() and not is_attacking():
+	if not is_dead:
+		if Input.is_action_just_pressed("attack_slash") and not is_landing() and not is_attacking():
 			attack()
-			
 		if not is_landing():
 			var horizontal_axis = Input.get_action_strength("right")-Input.get_action_strength("left")
-			movement.x = horizontal_axis*walk_speed
+			if Input.is_action_pressed("run"):
+				movement.x = horizontal_axis*run_speed
+				is_running = true
+			else:
+				movement.x = horizontal_axis*walk_speed
+				is_running = false
 			
 			if Input.is_action_just_pressed("jump") and is_on_floor():
 				movement.y = jump_speed
@@ -62,23 +68,24 @@ func _physics_process(delta):
 			movement.x = 0
 		
 		if esp32Esquerda == true:
-			movement.x = -1*walk_speed
+			movement.x = -1*run_speed
 		if esp32Direita == true:
-			movement.x = 1*walk_speed
+			movement.x = 1*run_speed
 	
 	move_and_slide_with_snap(movement, Vector2(0,2), Vector2.UP, true, 4, 0.9)
 	if not is_dead:
 		update_animations()
 
 func update_animations():
-	if mouse_position.x > 512:
-		$AnimatedSprite.scale.x = 1
-		$Swordhit/sword_strike.scale.x = 1
-		$Swordhit/sword_strike.position.x = 11.914
-	elif mouse_position.x < 512:
-		$AnimatedSprite.scale.x = -1
-		$Swordhit/sword_strike.scale.x = -1
-		$Swordhit/sword_strike.position.x = -11.914
+	if not is_attacking():
+		if mouse_position.x > 512 and not is_running or movement.x > 0 and is_running:
+			$AnimatedSprite.scale.x = 1
+			$Swordhit/sword_strike.scale.x = 1
+			$Swordhit/sword_strike.position.x = 11.914
+		elif mouse_position.x < 512 and not is_running or movement.x < 0 and is_running:
+			$AnimatedSprite.scale.x = -1
+			$Swordhit/sword_strike.scale.x = -1
+			$Swordhit/sword_strike.position.x = -11.914
 	
 	if life <= 0:
 		$AnimatedSprite.play("death")
@@ -88,7 +95,10 @@ func update_animations():
 		if last_movement_y > 500:
 			$AnimatedSprite.play("land")
 		elif abs(movement.x) > 0 and not is_attacking():
-			$AnimatedSprite.play("walking")
+			if is_running:
+				$AnimatedSprite.play("run")
+			else:
+				$AnimatedSprite.play("walk")
 		elif not is_landing() and not is_attacking():
 			$AnimatedSprite.play("idle")
 	elif not is_attacking() or movement.y == jump_speed:
@@ -102,10 +112,10 @@ func is_landing():
 	return last_animation == "land" and $AnimatedSprite.frame != 2
 
 func is_attacking():
-	return last_animation == "attack_Strike" and $AnimatedSprite.frame != 4
+	return last_animation == "attack_slash" and $AnimatedSprite.frame != 4
 
 func attack():
-	$AnimatedSprite.play("attack_Strike")
+	$AnimatedSprite.play("attack_slash")
 	last_animation = $AnimatedSprite.animation
 	$Swordhit/sword_strike.disabled = false
 	#print("on")
